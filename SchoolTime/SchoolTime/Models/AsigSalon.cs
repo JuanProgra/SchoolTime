f0edcb757bfc2206ab93849d64decdc6da1e44c3
JuanProgra <ljprogra911@gmail.com>
2015-06-12 11:02:05 -0600

    Interfaz

---
+++ SchoolTime/SchoolTime/Models/AsigSalon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolTime.Models
+{
+    public class AsigSalon
+    {
+        public String Codigo{get; set;}
+        public String Materia { get; set; }
+        public String Salon { get; set; }
+        public String Hora { get; set; }
+        public String Dia { get; set; }
+    }
+}
--- SchoolTime/SchoolTime/WebService1.asmx.cs
+++ SchoolTime/SchoolTime/WebService1.asmx.cs
@@ -46,5 +46,40 @@ namespace SchoolTime
             }
             return nombre;
         }
+        [WebMethod]
+        public List<AsigSalon> Get_horario(string codigo,string time)
+        {
+            DateTime timeD = Convert.ToDateTime(time);
+
+            AsigancionSalon asig = db.AsigancionSalons.FirstOrDefault(asi => asi.Grupo.Codigo == codigo && asi.Dia == timeD);
+
+            List<AsigSalon> asigancion = new List<AsigSalon>();
+            AsigSalon obj;
+            var culture = new System.Globalization.CultureInfo("es-ES");
+
+            if (asig != null)
+            {
+                obj = new AsigSalon();
+                obj.Codigo = asig.Grupo.Codigo;
+                obj.Hora = Convert.ToString(asig.Hora.TimeOfDay);
+                obj.Materia = asig.Materia.Nombre;
+                obj.Dia = culture.DateTimeFormat.GetDayName(asig.Dia.DayOfWeek);
+
+                asigancion.Add(obj);
+            }
+            return asigancion;
+        }
+        [WebMethod]
+        public String Get_Grupo(string usuairo)
+        {
+            String codigo = "";
+            AsignacionGrupo asGru = db.AsignacionGrupoes.FirstOrDefault(ast => ast.Usuario.NombreUsuario == usuairo);
+
+            if (asGru != null)
+            {
+                codigo = asGru.Grupo.Codigo;
+            }
+            return codigo;
+        }
     }
 }
